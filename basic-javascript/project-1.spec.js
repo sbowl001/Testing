99a0157827d90bf0c4ea8278158825a6150f9606
Stephanie Bowles <sbowl001@fiu.edu>
2018-06-27 22:43:33 +0800

    first test

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,10 @@
 const helpers = require('./project-1');
 
 // start testing!
+const multiplyByTen = num => {
+    return num * 10;
+  };
+it('should multiply a number by 10', () => {
+    const fifty = multiplyByTen(5)
+    expect(fifty).toBe(50)
+});
