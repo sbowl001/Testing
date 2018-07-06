0f7fdc5b316a6eaebc89490935437ad7ab13fad9
stephanie Bowles <sbowl001@fiu.edu>
2018-07-06 11:00:00 +0800

    two more tests

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,10 +1,122 @@
-const helpers = require('./project-1');
+const {multiplyByTen, subtractFive, areSameLength} = require('./project-1');
 
 // start testing!
-const multiplyByTen = num => {
-    return num * 10;
-  };
+ 
 it('should multiply a number by 10', () => {
     const fifty = multiplyByTen(5)
     expect(fifty).toBe(50)
 });
+
+ it('should subtract a number by 5 ', () => {
+     const five = subtractFive(10)
+     expect(five).toBe(5)
+ });
+
+
+it('should equate the length of two strings', ()=> {
+    const sameLength = areSameLength( 1, 1)
+    expect(sameLength).toBe(true)
+})
+ 
+/*
+const areEqual = (x, y) => {
+  return x === y;
+};
+
+const lessThanNinety = num => {
+  if (num < 90) {
+    return true;
+  }
+  return false;
+};
+
+const greaterThanFifty = num => {
+  if (num > 50) {
+    return true;
+  }
+  return false;
+};
+
+const add = (x, y) => {
+  return x + y;
+};
+
+const subtract = (x, y) => {
+  return x - y;
+};
+
+const divide = (x, y) => {
+  return x / y;
+};
+
+const multiply = (x, y) => {
+  return x * y;
+};
+
+const getRemainder = (x, y) => {
+  return x % y;
+};
+
+const isEven = num => {
+  if (num % 2 === 0) {
+    return true;
+  }
+  return false;
+};
+
+const isOdd = num => {
+  if (num % 2 === 0) {
+    return false;
+  }
+  return true;
+};
+
+const square = num => {
+  return num * num;
+};
+
+const cube = num => {
+  return num * num * num;
+};
+
+const raiseToPower = (num, exponent) => {
+  return num ** exponent;
+};
+
+const roundNumber = num => {
+  return Math.round(num);
+};
+
+const roundUp = num => {
+  return Math.ceil(num);
+};
+
+const addExclamationPoint = str => {
+  return (str += '!');
+};
+
+const combineNames = (firstName, lastName) => {
+  return `${firstName} ${lastName}`;
+};
+
+const getGreeting = name => {
+  return `Hello ${name}!`;
+};
+
+const getRectangleArea = (length, width) => {
+  return length * width;
+};
+
+const getTriangleArea = (base, height) => {
+  return 0.5 * base * height;
+};
+
+const getCircleArea = radius => {
+  return Math.PI * radius * radius;
+};
+
+const getRectangularPrismVolume = (length, width, height) => {
+  return width * height * length;
+};
+
+ */ 
