9db18b32c3922d731dce00afcfb0967b1e454bc2
Stephanie Bowles <sbowl001@fiu.edu>
2018-07-16 18:28:33 +0800

    from solution lecture

--- advanced-javascript/objects.spec.js
+++ advanced-javascript/objects.spec.js
@@ -1,36 +1,53 @@
 const objectFunctions = require('./objects');
 
-// whoops.. there is no test suite for this file. You'll simply just have to create one :/
+
+beforeEach(()=>{
+  obj = { a: 1, b: 2}
+});
+
 describe('objects', () => {
-  it('should be an object', () => {});
+  it('should be return keys', () => {
+    const keys = objectFunctions.keys(obj)
+    expect(keys).toEqual(['a', 'b'])
+ 
+  });
+  it('should return values', ()=> {
+    const values = objectFunctions.values(obj)
+    expect(values).toEqual([1, 2])
+  });
+
+  it('should return an object with mapped values', ()=> {
+    const cb = (item) => {
+      return item + 1;
+    }
+    const mapped = objectFunctions.mapObject(obj, cb);
+    expect(mapped).toEqual({ a: 2, b: 3})
+  })
+  it('should return an array with key value pairs', () => {
+    const arr = [['a', 1], ['b', 2]];
+    const pairs = objectFunctions.pairs(obj);
+    expect(pairs).toEqual(arr); 
+  })
+  it('should return an object with key/values inverted', () => {
+    const newObj = { 1: 'a', 2: 'b'}
+    const swapped = objectFunctions.invert(obj)
+    expect(swapped).toEqual(newObj);
+  })
+  it('should return an obj with default props added', ()=> {
+    const defaults = {
+      name: 'He',
+      a: 2
+    }
+    objectFunctions.defaults(obj, defaults)
+    expect(obj.name).toBe('He');
+    expect(obj.a).toBe(1);
+    expect(obj.a).not.toBe(2);
+  })
+
 });
 
 
-// const keys = (obj) => {
-//   return Object.keys(obj);
-// };
-
-// const values = (obj) => {
-//   return Object.keys(obj).map((key) => {
-//     return obj[key];
-//   });
-// };
-
-// const mapObject = (obj, cb) => {
-//   Object.keys(obj).forEach((key) => (obj[key] = cb(obj[key])));
-//   return obj;
-// };
-
-// const pairs = (obj) => Object.keys(obj).map((key) => [key, obj[key]]);
-
-// const invert = (obj) => {
-//   Object.keys(obj).forEach((key) => {
-//     const newKey = obj[key];
-//     obj[newKey] = key;
-//     delete obj[key];
-//   });
-//   return obj;
-// };
+ 
 
 // const defaults = (obj, defaultProps) => {
 //   Object.keys(defaultProps).forEach((key) => {
